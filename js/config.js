bf6a3202aa1b3886ae7bae237629626aeeaf4cee
wride78 <wride78@gmail.com>
2026-03-31 08:47:49 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
 export const SHEET_ID = atob("MTBNeGkzWndCejJiN0FnRi16RHJvUlpocWhUZnNHZE1EbWJPdldYaXYyUTg=");
-export const SHEET_NAME = "nodes";
+
+// 탭 이름 대신 gid 사용
+export const SHEET_GID = "0";
 
 export const NODES_URL =
-  `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&sheet=${encodeURIComponent(SHEET_NAME)}`;
+  `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&gid=${SHEET_GID}`;
 
 export const LOCAL_DEPTH = 1;
 export const SCALE_MIN = 0.35;
