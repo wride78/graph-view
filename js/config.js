5e6f1d65ab04cbe37182c1bad464ba25a98d9939
wride78 <wride78@gmail.com>
2026-03-31 09:07:02 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
-export const SHEET_ID = atob("MTBNeGkzWndCejJiN0FnRi16RHJvUlpocWhUZnNHZE1EbWJPdldYaXYyUTg=");
+export const SHEET_ID = "10Mxi3ZwBz2b7AgF-zDroRZhahTfsGdMDmbOvWXiv2Q8";
 export const SHEET_NAME = "nodes";
 export const SHEET_GID = "0";
 
-// 먼저 gid로 시도하고, 실패하면 sheet 이름으로 fallback
 export const NODES_URL_CANDIDATES = [
   `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&gid=${SHEET_GID}`,
   `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&sheet=${encodeURIComponent(SHEET_NAME)}`
