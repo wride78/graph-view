5a4e8ddc47c540b9be1a90e3d47edadd85fbffb3
wride78 <wride78@gmail.com>
2026-03-31 14:42:20 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const SHEET_ID="10Mxi3ZwBz2b7AgF-zDroRZhqhTfsGdMDmbOvWXiv2Q8";
+export const SHEET_ID="10Mxi3ZwBz2b7AgF-zDroRZhahTfsGdMDmbOvWXiv2Q8";
 export const URL_MAIN=`https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&gid=0`;
 export const URL_NODE=`https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&gid=1`;
 
